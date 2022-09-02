c25996691834445eb2d4a6d2a395f8d3ffe31a6a
alkaagrawal <alkaagrawal192@gmail.com>
2022-09-02 16:49:03 +0530

    DAO Completed

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xf20c9cF3d8D11AFC6a35F5cCfE7A2c7Ee500A3E7";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
